319274347aeed10e5509ecadaa4225690805bb2f
ndgndg91 <ndgndg91@gmail.com>
2020-09-07 22:33:55 +0900

    Modules: Import and Export

---
+++ 04-classes/05-modules/Driver.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Person_1 = require("./Person");
+var driver = new Person_1.Person('Dong-Gil', 'Nam');
+console.log("Driver : " + driver.firstName + " " + driver.lastName);
---
+++ 04-classes/05-modules/Driver.ts
@@ -0,0 +1,5 @@
+import { Person } from  './Person';
+
+let driver = new Person('Dong-Gil', 'Nam');
+
+console.log(`Driver : ${driver.firstName} ${driver.lastName}`)
